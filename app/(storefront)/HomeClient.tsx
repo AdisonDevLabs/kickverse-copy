10ebeaaad51b2a2d4692391d2982eec48ec71379
Adison-tech <adisoncheruiyot55@gmail.com>
2026-08-02 03:29:06 +0300

    hero section

--- app/(storefront)/HomeClient.tsx
+++ app/(storefront)/HomeClient.tsx
@@ -101,10 +101,10 @@ export default function HomeClient({ initialProducts, initialCategories, initial
             className="hero-image object-cover object-right md:object-center opacity-80 md:opacity-100"
           />
           {/* Mobile dark overlay for guaranteed readability */}
-          <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-black/80 via-black/40 to-transparent z-0" />
+          <div className="absolute inset-0 bg-black/40 z-0" />
           
-          {/* Desktop heavy black gradient on left side fading seamlessly to the image 
-          <div className="absolute inset-0 bg-gradient-to-r from-black via-black/80 to-transparent hidden md:block z-0" />*/}
+          {/* Desktop heavy black gradient on left side fading seamlessly to the image */}
+          <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-black/80 via-black/40 to-transparent z-0" />
         </div>
 
         {/* Content Wrapper - Changed items-start to items-center */}
